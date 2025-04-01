0a5275bcbf16a5064e2f479007c95b792c353bd4
Live session user <nutcracker.kda@gmail.com>
2025-04-01 01:18:33 +0000

    idk

--- linked_list.c
+++ linked_list.c
@@ -6,6 +6,7 @@ struct list_node *new_node(size_t value) { return NULL; }
 
 void insert_at_head(struct linked_list *list, size_t value) {
 	struct linked_list *temp = (struct linked_list*)malloc(sizeof(struct linked_list));
+	temp->head = (struct list_node*)malloc(sizeof(struct list_node));
 	temp->head->value = value;
 	temp->head->next = list->head;
 	list = temp;
@@ -14,6 +15,7 @@ void insert_at_head(struct linked_list *list, size_t value) {
 
 void insert_at_tail(struct linked_list *list, size_t value) {
 	struct linked_list *temp = (struct linked_list*)malloc(sizeof(struct linked_list));
+	temp->head = (struct list_node*)malloc(sizeof(struct list_node));
 	temp->head->value = value;
 	struct linked_list *evenMoreTemp = list;
 	while(evenMoreTemp->head->next != NULL)
@@ -37,7 +39,7 @@ size_t remove_from_tail(struct linked_list *list) {
 	}
 	val = temp->head->next->value;
 	temp->head->next = NULL;
-	return 0; }
+	return val; }
 
 void free_list(struct linked_list list) {
 	struct linked_list temp;
